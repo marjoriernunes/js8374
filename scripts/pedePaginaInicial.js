1265cb766d8e9f917643b4d6d3c51b0d836d09d4
Marjorie <marjorie.rnunes@gmail.com>
2019-06-18 22:43:46 -0300

    improve code

--- scripts/pedePaginaInicial.js
+++ scripts/pedePaginaInicial.js
@@ -1,20 +1,24 @@
-let paginaInicial = localStorage.getItem('paginaDefinida')
+const aceitouAnteriormente = JSON.parse(localStorage.getItem('aceitouSalvar'))
 
-if(!paginaInicial) {
-    paginaInicial = prompt("turu bom? Para qual site vamos @?")
-}
-
-if(paginaInicial) {
-    if(
-        paginaInicial.substring(0, 7) !== 'http://' &&
-        paginaInicial.substring(0, 8) !== 'https://'   
-    ) {
-        paginaInicial = 'http://' + paginaInicial
+if(aceitouAnteriormente === null) {
+    let paginaInicial = localStorage.getItem('paginaDefinida')
+    
+    if(!paginaInicial) {
+        paginaInicial = prompt("turu bom? Para qual site vamos @?")
     }
     
-    $janelaPrincipal.src = paginaInicial
-    $inputEndereco.value = paginaInicial
-    localStorage.setItem('paginaDefinida', paginaInicial)
+    if(paginaInicial) {
+        if(
+            paginaInicial.substring(0, 7) !== 'http://' &&
+            paginaInicial.substring(0, 8) !== 'https://'   
+        ) {
+            paginaInicial = 'http://' + paginaInicial
+        }
+        
+        $janelaPrincipal.src = paginaInicial
+        $inputEndereco.value = paginaInicial
+        localStorage.setItem('paginaDefinida', paginaInicial)
+    }
 }
 
 
