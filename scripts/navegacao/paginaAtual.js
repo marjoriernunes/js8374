17f43eefd7b0b8d01fc2b0c2bdc5ac5d52fc59dd
Marjorie <marjorie.rnunes@gmail.com>
2019-06-25 18:37:04 -0300

    feat: add new buttons - search

--- scripts/navegacao/paginaAtual.js
+++ scripts/navegacao/paginaAtual.js
@@ -2,8 +2,6 @@ import * as storagePaginaInicial from '/scripts/storage/paginaInicial.js'
 import { formataEndereco } from '/scripts/endereco/formataEndereco.js'
 import { carregar } from '/scripts/navegacao/carregar.js'
 
-//TODO criar módulo pra isso
-// Pegar pagina atual do localStorage
 const paginaAtual = sessionStorage.getItem('paginaAtual')
 
 const paginaPraCarregar = paginaAtual !== null
@@ -14,7 +12,31 @@ const enderecoCompleto = formataEndereco(paginaPraCarregar)
 carregar(enderecoCompleto)
 
 $janelaPrincipal.addEventListener('load', function salvaPaginaAtual() {
-    sessionStorage.setItem('paginaAtual', $janelaPrincipal.contentWindow.location.href)
+    const endereco = $janelaPrincipal.contentWindow.location.href
+    sessionStorage.setItem('paginaAtual', endereco)
+    listaSites.push(endereco)
+    posicao++
+})
+
+let listaSites = []
+
+let posicao = -1
+
+$botaoVoltar.addEventListener('click', function(){
+    if(listaSites.length !== 1) {
+        posicao--
+        carregar(
+            listaSites[posicao]
+        )  
+    }
+})
+$botaoAvancar.addEventListener('click', function() {
+    if(listaSites.length !== 1) {
+        posicao++
+        carregar(
+            listaSites[posicao]
+        )
+    }
 })
 
 
